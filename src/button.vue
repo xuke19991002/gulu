9393c6fa4a03df8ee4c4cbfd92d86d4858e31fd0
xuke <xk1242425209@163.com>
2020-08-28 23:52:54 +0800

    给button组件增加icon功能

--- src/button.vue
+++ src/button.vue
@@ -1,12 +1,33 @@
 <template>
-  <div>
-    <button class="g-button">按钮</button>
-  </div>
+  <!-- iconPosition:  icon-left | icon-right -->
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <i class="gicon">
+      <svg class="icon" aria-hidden="true" v-if="icon">
+        <use :xlink:href="`#icon-${icon}`" />
+      </svg>
+    </i>
+    <span class="content">
+      <slot></slot>
+    </span>
+  </button>
 </template>
 
 <script>
 export default {
   name: 'gButton',
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(val) {
+        return val !== 'left' || val !== 'right'
+      }
+    }
+  },
   data() {
     return {}
   },
@@ -24,14 +45,34 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-}
-.g-button:hover {
-  border-color: var(--border-color-hover);
-}
-.g-button:active {
-  background: var(--button-active-bg);
-}
-.g-button:focus {
-  outline: none;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .gicon {
+    order: 1; // flex 更改元素排列顺序 数字越小越靠前
+    margin-right: 5px;
+  }
+  > .content {
+    order: 2;
+  }
+  &:icon-right {
+    > .gicon {
+      margin-left: 5px;
+      order: 2;
+    }
+    > .content {
+      order: 1;
+    }
+  }
 }
 </style>
