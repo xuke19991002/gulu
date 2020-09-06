f1fea53c72fde98ce68fa9165dc42e0a9b283472
xuke <xk1242425209@163.com>
2020-09-06 17:34:42 +0800

    row 和 col 的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col" :class="[`g-col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    span: [Number, String]
+  },
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  background: grey;
+  border: 1px solid red;
+  width: 100%;
+  $class-prefix: g-col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+}
+</style>
---
+++ src/row.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.row {
+  display: flex;
+}
+</style>
