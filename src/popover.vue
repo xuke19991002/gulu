517b3ce9aba4134d2939829ea0c8894a1c033272
xuke <xk1242425209@163.com>
2020-09-19 23:23:03 +0800

    实现最简单的popover功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click="handlePopover">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="handlePopover">
+    <div class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
     <slot></slot>
@@ -9,35 +9,50 @@
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       visible: false
     }
   },
-  mounted() {
-
-  },
+  mounted() {},
   methods: {
-    handlePopover(){
+    handlePopover() {
       this.visible = !this.visible
+      if (this.visible) {
+        // 不同代码会在点击后立马执行监听函数 导致打开后监听到了click立马又给关闭了
+        // 异步 等待下一次点击 去监听
+        this.$nextTick(() => {
+          document.addEventListener('click', this.eventListenerCb)
+        })
+      }
+    },
+    eventListenerCb() {
+      this.visible = false
+      document.removeEventListener('click', this.eventListenerCb)
     }
+    /* 
+      未实现功能：
+        1：多种事件支持 比如 hover 上去
+        2：css的一个问题 如果popover外层元素存在overflow:hidden 会存在popover展示区域无法正常显示的问题
+          这也是为啥比较出名的UI库多倾向于直接把popover直接挂载到body上
+    */
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.popover{
+.popover {
   display: inline-flex;
   vertical-align: top;
   position: relative;
-  .content-wrapper{
+  .content-wrapper {
     position: absolute;
     bottom: 100%;
     left: 0;
     padding: 18px 20px;
     min-width: 150px;
     background: #fff;
-    box-shadow: 3px 3px 7px rgba(0,0,0,.07);
+    box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.07);
     word-break: break-all;
     border-radius: 4px;
   }
