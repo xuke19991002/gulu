0e2f16e2370e59124d4b98abd6a0b4c6467be0be
xuke <xk1242425209@163.com>
2020-09-20 22:46:24 +0800

    添加四个方向显示的支持

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="popover" @click.stop="handlePopoverClick">
-    <div ref="contentWrapper" class="popover-content-wrapper" v-if="visible">
+    <div
+      ref="contentWrapper"
+      class="popover-content-wrapper"
+      :class="contentWrapperClass"
+      v-if="visible"
+    >
       <div class="popover-arrow" ref="arrow"></div>
       <div class="popover-content-inner">
         <slot name="content"></slot>
@@ -15,22 +20,52 @@
 <script>
 import { offset } from './utils/index'
 export default {
+  props: {
+    position: {
+      type: String,
+      default: 'top',
+      validator(value) {
+        return ['top', 'bottom', 'left', 'right'].includes(value)
+      }
+    }
+  },
   data() {
     return {
       visible: false
     }
   },
-  mounted() {},
+  computed: {
+    contentWrapperClass() {
+      return [`content-position-${this.position}`]
+    }
+  },
   methods: {
     setPopoverPosition() {
       const contentWrapper = this.$refs.contentWrapper
       const triggerWrapper = this.$refs.triggerWrapper
       document.body.appendChild(contentWrapper)
       const { top, left } = offset(triggerWrapper)
-      // 使popover位于弹出层上方居中位置
-      contentWrapper.style.left =
-        left - contentWrapper.offsetWidth / 2 + triggerWrapper.offsetWidth / 2 + 'px'
-      contentWrapper.style.top = top - contentWrapper.offsetHeight - 13 + 'px'
+      const popoverPositionDescription = {
+        top: {
+          left: left - contentWrapper.offsetWidth / 2 + triggerWrapper.offsetWidth / 2,
+          top: top - contentWrapper.offsetHeight - 13
+        },
+        bottom: {
+          left: left - contentWrapper.offsetWidth / 2 + triggerWrapper.offsetWidth / 2,
+          top: top + triggerWrapper.offsetHeight + 13
+        },
+        left: {
+          left: left - contentWrapper.offsetWidth - 13,
+          top: top - (contentWrapper.offsetHeight - triggerWrapper.offsetHeight) / 2
+        },
+        right: {
+          left: left + triggerWrapper.offsetWidth + 13,
+          top: top - (contentWrapper.offsetHeight - triggerWrapper.offsetHeight) / 2
+        }
+      }
+      contentWrapper.style.left = popoverPositionDescription[this.position].left + 'px'
+      contentWrapper.style.top = popoverPositionDescription[this.position].top + 'px'
+
     },
     handlePopoverClick() {
       if (this.$refs.triggerWrapper.contains(event.target)) {
@@ -85,27 +120,53 @@ export default {
 }
 .popover-content-wrapper {
   position: absolute;
-}
-.popover-content-inner {
-  padding: 18px 20px;
-  min-width: 150px;
-  background: #fff;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
-  word-break: break-all;
-  border-radius: 4px;
-}
-.popover-arrow {
-  position: absolute;
-  bottom: -3.8px;
-  left: 50%;
-  width: 8px;
-  height: 8px;
-  border-width: 4px;
-  border-color: transparent;
-  border-right-color: #fff;
-  border-bottom-color: #fff;
-  box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.07);
-  transform: translateX(-50%) rotate(45deg);
-  background: #fff;
+  .popover-content-inner {
+    padding: 18px 20px;
+    min-width: 150px;
+    background: #fff;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+    word-break: break-all;
+    border-radius: 4px;
+  }
+  .popover-arrow {
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    border-width: 4px;
+    border-color: transparent;
+    border-right-color: #fff;
+    border-bottom-color: #fff;
+    box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.07);
+    background: #fff;
+  }
+  
+  &.content-position-top{
+    .popover-arrow{
+      bottom: -3.8px;
+      left: 50%;
+      transform: translateX(-50%) rotate(45deg);
+    }
+  }
+  &.content-position-bottom{
+    .popover-arrow{
+      top: -3.8px;
+      left: 50%;
+      transform: translateX(-50%) rotate(-135deg);
+    }
+  }
+  &.content-position-left {
+    .popover-arrow{
+      right: -3.8px;
+      top: 50%;
+      transform: translateY(-50%) rotate(-45deg);
+    }
+  }
+  &.content-position-right {
+    .popover-arrow{
+      left: -3.8px;
+      top: 50%;
+      transform: translateY(-50%) rotate(-225deg);
+    }
+  }
 }
 </style>
